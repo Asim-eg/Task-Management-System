0b5aece6fca7972b19eab0fd4dc8eecb6f872d19
Asim Altaf <asim.altaf@zixel.cn>
2024-01-04 15:57:33 +0500

    【Update】Added Frontend and Updated Model

---
+++ Fontend/script.js
@@ -0,0 +1,11 @@
+let Task = {
+  id: number,
+  name: string,
+  description: string,
+  isCompleted: Boolean,
+  createdAt: string,
+  updatedAt: string,
+  createdBy: string,
+};
+
+function CreateTasl(Task) {}
--- Models/tasks.go
+++ Models/tasks.go
@@ -1,13 +1,11 @@
 package models
 
-import "time"
-
 type Tasks struct {
-	Id          int       `json:"id" bson:"id"`
-	Name        string    `json:"name" bson:"name"`
-	Description string    `json:"description" 	bson:"description"`
-	IsCompleted bool      `json:"isCompleted" 		bson:"isCompleted"`
-	CreatedAt   time.Time `json:"createdAt" 		bson:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt" 		bson:"updatedAt"`
-	CreatedBy   string    `json:"author" 			bson:"author"`
+	Id          int    `json:"id" bson:"id"`
+	Name        string `json:"name" bson:"name"`
+	Description string `json:"description" 	bson:"description"`
+	IsCompleted bool   `json:"isCompleted" 		bson:"isCompleted"`
+	CreatedAt   string `json:"createdAt" 		bson:"createdAt"`
+	UpdatedAt   string `json:"updatedAt" 		bson:"updatedAt"`
+	CreatedBy   string `json:"author" 			bson:"author"`
 }
