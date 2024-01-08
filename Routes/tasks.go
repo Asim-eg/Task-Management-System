430ae68fcf4d70080cf8f3e6b9709d10346bef01
Asim Altaf <asim.altaf@zixel.cn>
2024-01-08 18:36:50 +0500

    【Update】Served HTML files with Backend

--- Routes/tasks.go
+++ Routes/tasks.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	controller "TaskManage/Controller"
+	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -17,12 +18,14 @@ func Init() *gin.Engine {
 	// Define API routes
 	api := router.Group("/tasks")
 	{
-		api.GET("/", controller.GetTasks)
-		api.POST("/", controller.CreateTask)
+		api.GET("/get", controller.GetTasks)
+		api.POST("/create", controller.CreateTask)
 		api.PUT("/:id", controller.UpdateTask)
 		api.DELETE("/:id", controller.DeleteTask)
 		api.GET("/:id", controller.GetTaskById)
 	}
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("Frontend/index.html"))))
 
 	// Run the server
 	router.Run(":8080")
